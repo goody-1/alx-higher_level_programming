01949b6eecc70407c342f0105e7c53f35c71bf04
Goodness <atundeg@gmail.com>
2021-11-16 23:54:30 +0100

    advanced task - 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    copy = ""
+
+    if n > -1 and n <= len(str) - 1:
+        for i in range(len(str)):
+            if i != n:
+                copy = copy + str[i]
+        return copy
+    else:
+        return str
