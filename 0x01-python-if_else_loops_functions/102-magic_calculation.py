8823dc79fe0c4c740b3f282474df1064282fa24b
Goodness <atundeg@gmail.com>
2021-11-17 02:40:54 +0100

    advanced task - 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+def magic_calculation(a, b, c):
+    """magic calculation 2 for bytecode"""
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
