1c23deeec88f0d4899e731c9e7cfc5010e9a3713
Goodness <atundeg@gmail.com>
2021-12-01 18:01:01 +0100

    handling zero dictionary length

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     student = []
     score = []
 
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     for key, value in a_dictionary.items():
