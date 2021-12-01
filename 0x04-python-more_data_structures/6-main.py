ab0d60490b5fca17ed53cb4193153c30080f5600
Goodness <atundeg@gmail.com>
2021-12-01 17:45:57 +0100

    project 0x04 - more data structures on python

---
+++ 0x04-python-more_data_structures/6-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': ['Low game', "Low level", "Age gown"], 'ids': [2, 1, 3] }
+print_sorted_dictionary(a_dictionary)
