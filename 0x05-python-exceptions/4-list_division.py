6d63c62ec8b7ff5d5d255614148694e0219cd5e1
Goodness <atundeg@gmail.com>
2021-12-06 18:06:31 +0100

    removed unused  variable

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,8 +4,6 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
 
-    len = max(my_list_1, my_list_2)
-
     for i in range(list_length):
         try:
             result.append(my_list_1[i] / my_list_2[i])
