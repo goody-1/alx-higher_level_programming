4f51ad8d599b15b941785b01ddc00702647e5dbd
Goodness <atundeg@gmail.com>
2021-12-06 12:24:52 +0100

    project 0x05 - python-exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        i -= 1
+        pass
+    print()
+
+    return i + 1
