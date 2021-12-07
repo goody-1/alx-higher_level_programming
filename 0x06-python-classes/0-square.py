1ac70a1bbb75576edb897b7abd1b22b55a4752b4
Goodness <atundeg@gmail.com>
2021-12-07 15:30:46 +0100

    trying to satisfy all __doc__

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""This module contains an empty class defining a square
+
+Class:
+    Square: empty square class
+"""
+
 
 class Square():
     """An empty class that defines a square
