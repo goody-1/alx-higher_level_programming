b4949d646cead7e893b57e20b55a7690b0421412
Goodness <atundeg@gmail.com>
2021-12-07 16:58:31 +0100

    task 5 - printing the squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+import sys
+
+"""This module contains a class - Square
+This is based on "4-square" but now prints the square
+"""
+
+
+class Square():
+    """Square class with private attribute - size
+    size must be an integer, TypeError and ValueError exceptions are raised
+    when they occur
+
+    methods:
+        set_size - sets the size of the square
+        get_size - gets the size of the square
+    """
+
+    def __init__(self, size=0):
+        """Initialize object
+        Instantiation with integer size which is optional
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get square size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set square size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """This returns the current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """This function prints the square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="", file=sys.stdout)
+                print()
