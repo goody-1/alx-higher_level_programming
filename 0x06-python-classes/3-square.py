72a3ba37e0f325a3100d6d0c9c9ba2f4e4456dd1
Goodness <atundeg@gmail.com>
2021-12-07 16:21:55 +0100

    task 3 - compute area of square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""This module contains a class - Square
+This is based on "2-square" but now computes the area
+"""
+
+
+class Square():
+    """Square class with private attribute - size
+    size must be an integer, TypeError and ValueError exceptions are raised
+    when they occur
+
+    methods:
+        set_size - sets the size of the square
+        get_size - gets the size of the square
+    """
+
+    def __init__(self, size=0):
+        """Initialize object
+        Instantiation with integer size which is optional
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def set_size(self, size):
+        """Set square size
+        """
+        self.__size = size
+
+    def get_size(self):
+        """Get square size
+        """
+        return self.__size
+
+    def area(self):
+        """This returns the current square area
+        """
+        return self.__size ** 2
