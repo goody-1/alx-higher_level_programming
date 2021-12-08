f5c14389faac92fe879e1f3b8f27ccbb5d498148
Goodness <atundeg@gmail.com>
2021-12-09 00:18:08 +0100

    last advanced task - byte code

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,12 +10,12 @@ class MagicClass():
     def __init__(self, radius):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
         """Return the area"""
-        return self.radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Return the circumference"""
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
