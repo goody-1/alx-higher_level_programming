9e8a27a2e6e3c079ea7f102db11c2f2198f0ce64
Goodness <atundeg@gmail.com>
2021-12-09 00:16:51 +0100

    last advanced task - byte code

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import math
+
+
+class MagicClass():
+    """This is a class to test for byte code
+    """
+
+    def __init__(self, radius):
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """Return the area"""
+        return self.radius ** 2 * math.pi
+
+    def circumference(self):
+        """Return the circumference"""
+        return 2 * math.pi * self.radius
