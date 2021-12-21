61cb079747e0527ee73b82706323f526e05704b6
Goodness <atundeg@gmail.com>
2021-12-21 18:52:30 +0100

    typo fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -13,4 +13,4 @@ class BaseGeometry():
         """
         Raises an Exception if area is not implemented
         """
-        raise Exception("area() is not implementned")
+        raise Exception("area() is not implemented")
