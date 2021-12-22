cd56bef979f664608ac9eae82f059ec497956ac6
Goodness <atundeg@gmail.com>
2021-12-22 14:29:57 +0100

    added documentation

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
-import sys
-import io
+"""
+Demonstrates Log parsing
 
-dictstatus = {}
-totalsize = 0
-totalcount = 0
+script that reads stdin line by line and computes metrics:
+
+    -   Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+        <status code> <file size>
+    -   Each 10 lines and after a keyboard interruption (CTRL + C), prints
+        those statistics since the beginning:
+        -   Total file size: File size: <total size>
+        -   where is the sum of all previous (see input format above)
+        -   Number of lines by status code:
+            -   possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+            -   if a status code doesn’t appear, don’t print anything for this
+                status code
+            -   format: <status code>: <number>
+            -   status codes should be printed in ascending order
+"""
+import sys
+
+status_dict = {}
+total_size = 0
+count = 0
+
 for line in sys.stdin:
     split = line.split()
     status = split[-2]
-    totalsize += int(split[-1])
-    if status in dictstatus.keys():
-        dictstatus[status] += 1
+    total_size += int(split[-1])
+    if status in status_dict.keys():
+        status_dict[status] += 1
     else:
-        dictstatus[status] = 1
-    totalcount += 1
-    if totalcount == 10:
-        sortme = sorted(dictstatus.keys())
-        print("File size:", totalsize)
+        status_dict[status] = 1
+    count += 1
+    if count == 10:
+        sortme = sorted(status_dict.keys())
+        print("File size:", status_dict)
         for keys in sortme:
-            print("{}: {}".format(keys, dictstatus[keys]))
-        totalcount = 0
+            print("{}: {}".format(keys, status_dict[keys]))
+        count = 0
         continue
