e0e1738abcdf23fc764586b0d553e537e54986f3
Goodness <atundeg@gmail.com>
2022-02-03 19:34:51 +0100

    change comment type

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,13 +2,11 @@
 
 -- Script that creates the table force_name on the server
 
-/*
- * force_name description:
- *		id INT
- *		name VARCHAR(256) can’t be null
- * The database name will be passed as an argument of the mysql command
- * If the table force_name already exists, your script should not fail
- */
+-- force_name description:
+--		id INT
+--		name VARCHAR(256) can’t be null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS `force_name` (
 	`id` INT,
