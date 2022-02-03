680fef7d544ca76c8dbd8f104a2f4af92e6e7c23
Goodness <atundeg@gmail.com>
2022-02-03 22:15:28 +0100

    task 11: Genre ID for all shows, task 10 fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,15 +1,13 @@
-/* Genre ID by show */
+/* Genre ID for all shows */
 
--- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- Script that lists all shows contained in hbtn_0d_tvshows
 
 --	Each record should display: tv_shows.title - tv_show_genres.genre_id
 --	Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --	only one SELECT statement
 
-
   SELECT tv_shows.title, tv_show_genres.genre_id
   	FROM tv_shows
-    JOIN tv_show_genres
+ 	LEFT JOIN tv_show_genres
 	  ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC,
-  	tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
