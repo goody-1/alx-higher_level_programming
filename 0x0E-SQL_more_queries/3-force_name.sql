8ee9c6aa999e0171d1ea159e0ac069234a3da5fd
Goodness <atundeg@gmail.com>
2022-02-03 19:08:11 +0100

    attempt to fix checks

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -10,7 +10,7 @@
  * If the table force_name already exists, your script should not fail
  */
 
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS `force_name` (
+	`id` INT,
+	`name` VARCHAR(256) NOT NULL
 	);
