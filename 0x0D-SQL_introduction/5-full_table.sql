20636227a11d77fcff57186020cab5727fd5d063
Goodness <atundeg@gmail.com>
2022-02-03 13:08:37 +0100

    changed comment type

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 /* Full description */
 
-/* prints the full description of the table first_table from the database hbtn_0c_0 */
+-- prints the full description of the table first_table from the database hbtn_0c_0
 
 SHOW COLUMNS FROM first_table;
