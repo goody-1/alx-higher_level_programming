920bec2ffa7136a000e301d7a71632aa227ea99e
goody-1 <atundeg@gmail.com>
2024-03-14 20:20:25 +0100

    convert bytecode to python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(a, b, c):
+    from magic_calculation_102 import add, sub
+    """magic calculation 3 for bytecode"""
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
