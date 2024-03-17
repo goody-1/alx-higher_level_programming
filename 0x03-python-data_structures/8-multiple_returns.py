dad86299aea47f78e4381cc56ca679e2abc8ae6d
goody-1 <atundeg@gmail.com>
2024-03-17 22:18:03 +0100

    return multiple values from function

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = None
+
+    if length != 0:
+        first_char = sentence[0]
+
+    return length, first_char
