64701f8986ac0e52f23a4c27e942aff7091846ca
goody-1 <atundeg@gmail.com>
2024-04-02 16:20:00 +0100

    test function pointer

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
