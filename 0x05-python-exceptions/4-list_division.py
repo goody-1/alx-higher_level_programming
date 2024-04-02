2374cfc6806c216a188ca33ca96ab6f00ab09b00
goody-1 <atundeg@gmail.com>
2024-04-02 19:56:59 +0100

    split combined except'

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,8 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result = 0
-        except (TypeError, IndexError):
-            print("wrong type or out of range")
+        except (TypeError):
+            print("wrong type")
+            result = 0
+        except (IndexError):
+            print("out of range")
             result = 0
         finally:
             result_list.append(result)
