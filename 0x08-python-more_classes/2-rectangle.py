d68be38a542c4460d57193549454cd694373b8c7
goody-1 <atundeg@gmail.com>
2024-04-08 23:21:39 +0100

    width must not be zero neither must height

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -69,4 +69,6 @@ class Rectangle:
         Returns:
             The perimeter of the rectangle.
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
