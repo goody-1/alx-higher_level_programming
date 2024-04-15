233121448385e6d03abca16a789acd103bc47e6c
goody-1 <atundeg@gmail.com>
2024-04-15 21:58:12 +0100

    lookup -dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Contains `lookup` function """
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+
+    Parameters:
+    obj (object): The object to inspect
+
+    Returns:
+    list: A list of strings representing the attributes and methods of the object
+    """
+    return dir(obj)
