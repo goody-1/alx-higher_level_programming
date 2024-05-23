370e80bd0928eebb68028cf4e610790079d4dd53
goody-1 <atundeg@gmail.com>
2024-05-23 20:50:49 +0100

    do task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,18 @@
+/* Cities table */
+
+-- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)  on the server.
+
+--	cities description:
+--		id INT, auto generated, can’t be null and is a primary key
+--		state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+--		name VARCHAR(256) can't be null
+
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+	);
