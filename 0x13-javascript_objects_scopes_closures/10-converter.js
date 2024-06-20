2875c38325fc90fec427c4e7777c35acb1d756cb
goody-1 <atundeg@gmail.com>
2024-06-20 02:38:29 +0100

    do task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+Write a function that converts a number from base 10 to another base passed as argument:
+
+- Prototype: exports.converter = function (base)
+You are not allowed to import any file
+You are not allowed to declare any new variable (var, let, etc..)
+
+*/
+
+module.exports.converter = function (base) {
+  return (number) => {
+    return (number.toString(base));
+  };
+};
