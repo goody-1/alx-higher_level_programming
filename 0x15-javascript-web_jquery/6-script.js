abe8de738b2aea0e49690b303b29aa9595bc91c5
goody-1 <atundeg@gmail.com>
2024-08-11 22:48:32 +0100

    satisfy js semistandard

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('DIV#update_header').click(function () {
-        $('header').text('New Header!!!');
-    });
+  $('DIV#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
 });
